fda7aa909817b4062387fa93461293066a17974d
sntNicolAs <sntnicolas@gmail.com>
2025-09-07 17:16:35 +0300

    перемещение

--- Guide/python/args.py
+++ Guide/python/args.py
@@ -1,8 +1,13 @@
-def print_info(**kwargs):
+def print_info1(**kwargs):
     for key, value in kwargs.items():
         print(f"{key} = {value}")
 
-print_info(name="Коля", age=25, city="Москва")
+
+def print_info2(**kwargs):
+    print(*[f"{key} = {value}" for key, value in kwargs.items()], sep='\n')
+
+
+print_info2(name="Коля", age=25, city="Москва")
 
 
 def demo(a, b=10, *args, **kwargs):
@@ -11,6 +16,29 @@ def demo(a, b=10, *args, **kwargs):
     print("args:", args)
     print("kwargs:", kwargs)
 
+
 demo(1, 2, 3, 4, x=5, y=6)
 demo(7)
 demo(1, 2, 3, 4, mort=2)
+
+
+def process_data(*args, **kwargs):
+    squared_sum = sum(map(lambda x: x ** 2, args))
+    print(f"Сумма аргументов: {squared_sum}")
+    print(*[f"{key} = {value}" for key, value in kwargs.items()], sep="\n")
+
+process_data(1, 2, 3, a=10, b=20)
+
+def analyze_data(*args, **kwargs):
+    # squares = (lambda x: x ** 2, args)
+    # print(squares)
+    # print(lambda x: x ** 2, args)
+
+    list_squares: [x ** 2 for x in args]
+    print(list_squares)
+    print(f"Сумма квадратов : {sum(list_squares)}")
+    print(f"Макс число : {max(args)}")
+    print(f"{key} = {value if arg ** 2 if arg ")
+
+
+analyze_data(1, 2, 3, a=5, b="hello", c=10)
